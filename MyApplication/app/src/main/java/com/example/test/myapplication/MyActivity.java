e7f9fcb2f81b3a487e880e7461e7effa57bae854
test <test@idea-PC-jerry>
2014-10-08 22:55:40 +0800

    test app

---
+++ MyApplication/app/src/main/java/com/example/test/myapplication/MyActivity.java
@@ -0,0 +1,64 @@
+package com.example.test.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.provider.Telephony.Sms;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Button button = (Button)this.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MyActivity.this.getApplicationContext(),"test",Toast.LENGTH_LONG);
+//                Intent intent =
+//                        new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+//                intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
+//                        "com.example.test.myapplication");
+//                startActivity(intent);
+                Context context = MyActivity.this.getApplicationContext();
+                String defaultSmsApp = Telephony.Sms.getDefaultSmsPackage(context);
+                Intent intent = new Intent(Sms.Intents.ACTION_CHANGE_DEFAULT);
+                intent.putExtra(Sms.Intents.EXTRA_PACKAGE_NAME, context.getPackageName());
+                startActivity(intent);
+
+                Toast.makeText(MyActivity.this.getApplicationContext(), "Deleted.", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
